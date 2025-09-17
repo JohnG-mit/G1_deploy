b718decb9f001793160cd584cb0452557c4edb4a
JohnG-mit <johng-mit@outlook.com>
2025-09-17 15:59:51 +0800

    feat: add complete unitree python sdk

---
+++ deploy_real/unitree_sdk2_python/unitree_sdk2py/comm/motion_switcher/motion_switcher_api.py
@@ -0,0 +1,38 @@
+"""
+" service name
+"""
+MOTION_SWITCHER_SERVICE_NAME = "motion_switcher"
+
+
+"""
+" service api version
+"""
+MOTION_SWITCHER_API_VERSION = "1.0.0.1"
+
+
+"""
+" api id
+"""
+MOTION_SWITCHER_API_ID_CHECK_MODE = 1001
+MOTION_SWITCHER_API_ID_SELECT_MODE = 1002
+MOTION_SWITCHER_API_ID_RELEASE_MODE = 1003
+MOTION_SWITCHER_API_ID_SET_SILENT = 1004
+MOTION_SWITCHER_API_ID_GET_SILENT = 1005
+
+# """
+# " error code
+# """
+# # client side
+# SPORT_ERR_CLIENT_POINT_PATH = 4101
+# # server side
+# SPORT_ERR_SERVER_OVERTIME = 4201
+# SPORT_ERR_SERVER_NOT_INIT = 4202
+# UT_SWITCH_ERR_PARAMETR,         7001,  "parameter is invalid.")
+# UT_SWITCH_ERR_BUSY,             7002,  "switcher is busy.")
+# UT_SWITCH_ERR_EVENT,            7003,  "event is invalid.")
+# UT_SWITCH_ERR_NAME,             7004,  "name or alias is invalid.")
+# UT_SWITCH_ERR_CMD,              7005,  "name or alias is invalid.")
+# UT_SWITCH_ERR_EXEC_CHECK,       7006,  "check cmd execute error.")
+# UT_SWITCH_ERR_EXEC_SELECT,      7007,  "select cmd execute error.")
+# UT_SWITCH_ERR_EXEC_RELEASE,     7008,  "release cmd execute error.")
+# UT_SWITCH_ERR_CUSTOMIZE,        7009,  "save customize data error.")
---
+++ deploy_real/unitree_sdk2_python/unitree_sdk2py/g1/arm/g1_arm_action_client.py
@@ -0,0 +1,74 @@
+import json
+import sys
+import os
+
+# --- Start of fix ---
+# Add the project root to sys.path to resolve imports.
+# The project root is 3 levels up from this file's directory, which is 'deploy_real'.
+project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..', '..'))
+sys.path.insert(0, project_root)
+# --- End of fix ---
+
+from unitree_sdk2py.rpc.client import Client
+from unitree_sdk2py.g1.arm.g1_arm_action_api import *
+from unitree_sdk2py.core.channel import ChannelFactoryInitialize # <--- 导入初始化函数
+
+action_map = {
+    "release arm": 99,
+    "two-hand kiss": 11,
+    "left kiss": 12,
+    "right kiss": 13,
+    "hands up": 15,
+    "clap": 17,
+    "high five": 18,
+    "hug": 19,
+    "heart": 20,
+    "right heart": 21,
+    "reject": 22,
+    "right hand up": 23,
+    "x-ray": 24,
+    "face wave": 25,
+    "high wave": 26,
+    "shake hand": 27,
+}
+
+
+"""
+" class SportClient
+"""
+class G1ArmActionClient(Client):
+    def __init__(self):
+        super().__init__(ARM_ACTION_SERVICE_NAME, False)
+
+    def Init(self):
+        # set api version
+        self._SetApiVerson(ARM_ACTION_API_VERSION)
+
+        # regist api
+        self._RegistApi(ROBOT_API_ID_ARM_ACTION_EXECUTE_ACTION, 0)
+        self._RegistApi(ROBOT_API_ID_ARM_ACTION_GET_ACTION_LIST, 0)
+
+    ## API Call ##
+    def ExecuteAction(self, action_id: int):
+        p = {}
+        p["data"] = action_id
+        parameter = json.dumps(p)
+        code, data = self._Call(ROBOT_API_ID_ARM_ACTION_EXECUTE_ACTION, parameter)
+        return code
+    
+    def GetActionList(self):
+        p = {}
+        parameter = json.dumps(p)
+        code, data = self._Call(ROBOT_API_ID_ARM_ACTION_GET_ACTION_LIST, parameter)
+        if code == 0:
+            return code, json.loads(data)
+        else:
+            return code, None
+
+# ChannelFactoryInitialize(0, sys.argv[1] if len(sys.argv) > 1 else "")
+# armc = G1ArmActionClient()
+# armc.Init()
+# code, action_list = armc.GetActionList()
+# print(action_list)
+# res = armc.ExecuteAction(action_map["shake hand"])
+# print(res)
